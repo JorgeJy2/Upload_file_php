47918fa866aed4a9a730b2366c375639d02e0c73
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2019-12-15 20:12:56 -0600

    feat: add materialize

--- subir.php
+++ subir.php
@@ -10,35 +10,29 @@
     <?php
         if(isset($_POST['action'])){
             if($_POST['action']=='upload'){
-                //obtener el nombre del archivo
-                $tamano = $_FILES['archivo']['size'];
-                $tipo = $_FILES['archivo']['type'];
+                //Get info file.
+                $file_size = $_FILES['archivo']['size'];
+                $file_type = $_FILES['archivo']['type'];
                 $archivo = $_FILES['archivo']['name'];
                 $prefijo =  substr(md5(uniqid(rand())),0,6);
-                echo($tamano);
-                echo($tipo);
+                echo($file_size);
+                echo($file_type);
                 echo($archivo);
                 echo($prefijo);
                 if($archivo != ""){
                     //Guardamos el archivo
-                    $destino = "files/".$prefijo."_".$archivo;
-                    echo($destino);
+                    $destino = "images/".$prefijo."_".$archivo;
+                    // echo($destino);
                     if(copy($_FILES['archivo']['tmp_name'],$destino)){
                       echo  $status = "Archivo subido: <b> ".$archivo." </b>";
-                    } else {
-                        $status = "Error al subir el archivo";
-                    }
-                }else {
-                    $status = "Error al subirkfkk";
-                } 
-            }else {
+                    } else
+                        echo 'Error al subir el archivo';
+                }else 
+                    echo 'Error al subir';
+            }else
                 echo('upload');
-            }
-        }else {
+        }else
             echo('action');
-        }
-
     ?>
-    
 </body>
 </html>
