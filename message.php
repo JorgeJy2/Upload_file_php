eac6828844cc76f0968dfac0cdda3e8b3dbe4cfa
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2019-12-19 00:12:31 -0600

    feat: message in upload

--- message.php
+++ message.php
@@ -12,4 +12,18 @@ function showMsgError($message, $href)
   })";
   echo "</script>";
 }
+
+
+function showMsgOk($message, $href){
+    echo "<script>";
+    echo "
+    Swal.fire(
+      'Good!',
+      '$message',
+      'success'
+    ).then(()=>{
+      window.location = '$href';
+    });";
+    echo "</script>";
+}
 ?>
--- subir.php
+++ subir.php
@@ -1,50 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>upload</title>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>upload</title>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 </head>
+
 <body>
-    <?php
-        if(isset($_POST['action'])){
-            if($_POST['action']=='upload'){
-                //Get info file.
-                $file_size = $_FILES['archivo']['size'];
-                $file_type = $_FILES['archivo']['type'];
-                $archivo = $_FILES['archivo']['name'];
-                // $prefijo =  substr(md5(uniqid(rand())),0,6);
-                echo($file_size);
-                echo($file_type);
-                echo($archivo);
-                // echo($prefijo);
-                if($archivo != ""){
-                    //Guardamos el archivo
-                    // $destino = "images/".$prefijo."_".$archivo;
-                    $destino = "images/$archivo";      
-                    // echo($destino);
-                    if(copy($_FILES['archivo']['tmp_name'],$destino)){
-                    //   header('Location: ./view_image.php');      
-                      echo "<script>
-                      Swal.fire(
-                        'Good!',
-                        'Image upload!',
-                        'success'
-                      ).then(()=>{
-                        window.location = './view_image.php';
-                      });
-                      </script>";
-                    } else
-                        echo 'Error al subir el archivo';
-                }else 
-                    echo 'Error al subir';
-            }else
-                echo('upload');
-        }else
-            echo('action');
-    ?>
+  <?php
+  include './message.php';
+
+  if (isset($_POST['action'])) {
+    if ($_POST['action'] == 'upload') {
+      //Get info file.
+      $file_size = $_FILES['archivo']['size'];
+      $file_type = $_FILES['archivo']['type'];
+      $archivo = $_FILES['archivo']['name'];
+      // $prefijo =  substr(md5(uniqid(rand())),0,6);
+      // echo($file_size);
+      // echo($file_type);
+      // echo($archivo);
+      // echo($prefijo);
+      if ($archivo != "") {
+        //Guardamos el archivo
+        // $destination = "images/".$prefijo."_".$archivo;
+        $destination = "images/$archivo";
+        // echo($destination);
+        if (copy($_FILES['archivo']['tmp_name'], $destination))
+          //   header('Location: ./view_image.php');
+          showMsgOk('Image upload!', './view_image.php');
+        else
+          showMsgError('Could not delete', './index.html');
+      } else
+        showMsgError('the file is empy', './index.html');
+    } else
+      showMsgError('the acction is no upload', './index.html');
+  } else
+    showMsgError('the file is empy', './index.html');
+  ?>
 
 </body>
+
 </html>
